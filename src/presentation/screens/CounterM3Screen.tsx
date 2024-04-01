6d4d6c911d456cd80d12b6ac5771c983011b613c
AndresSanchezC <andressanchez@elecsis.co>
2024-04-01 17:11:13 -0500

    MYFirstApp

---
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -0,0 +1,24 @@
+import {useState} from 'react';
+import {Text, View} from 'react-native';
+import {globalStyles} from '../theme/globas.styles';
+import {FAB} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/Ionicons'
+
+export const CounterM3Screen = () => {
+  const [count, setCount] = useState(1);
+
+  return (
+    <View style={globalStyles.centerContainer}>
+      <Text style={globalStyles.title}>{count}</Text>
+      
+      {/* <Icon name ="accessibility-outline" size={35}/> */}
+      <FAB
+        icon="add"
+        // label="+1"
+        style={globalStyles.fab}
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => setCount(0)}
+      />
+    </View>
+  );
+};
